3c4f81c641bd502ca73f343de473e7010b00beb6
Neider Sinisterra <neidersinisteraa100@gmail.com>
2026-01-05 10:51:35 -0500

    [FIX] route.routes

--- backend/src/routes/route.routes.ts
+++ backend/src/routes/route.routes.ts
@@ -8,15 +8,15 @@ import { requireOwner } from "../middlewares/requireOwner.js";
 
 const router = Router();
 
-/* ================= PUBLIC ================= */
+/* ================= PROTECTED ================= */
 
-// LISTAR RUTAS DE UNA EMPRESA (PÚBLICO)
+// LISTAR RUTAS DE UNA EMPRESA (OWNER)
 router.get(
   "/company/:companyId",
+  requireAuth,
+  requireOwner,
   getCompanyRoutes
 );
-
-/* ================= PROTECTED ================= */
 
 // CREAR RUTA → SOLO OWNER
 router.post(
@@ -27,3 +27,36 @@ router.post(
 );
 
 export default router;
+
+
+
+
+// import { Router } from "express";
+// import {
+//   createRoute,
+//   getCompanyRoutes,
+// } from "../controllers/route.controller.js";
+// import { requireAuth } from "../middlewares/requireAuth.js";
+// import { requireOwner } from "../middlewares/requireOwner.js";
+
+// const router = Router();
+
+// /* ================= PUBLIC ================= */
+
+// // LISTAR RUTAS DE UNA EMPRESA (PÚBLICO)
+// router.get(
+//   "/company/:companyId",
+//   getCompanyRoutes
+// );
+
+// /* ================= PROTECTED ================= */
+
+// // CREAR RUTA → SOLO OWNER
+// router.post(
+//   "/",
+//   requireAuth,
+//   requireOwner,
+//   createRoute
+// );
+
+// export default router;
